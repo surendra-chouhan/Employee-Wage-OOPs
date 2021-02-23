f37c49e381f6ab509e69ece109c7981b92a6348b
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-23 20:38:26 +0530

    Added code for part time employee

--- empWage.java
+++ empWage.java
@@ -1,16 +1,21 @@
 public class empWage {
 
-	public static final int IS_PRESENT = 1;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
 	public static final int EMP_WAGE_PER_HOUR = 20;
 
 	public static void calculate_wage() {
 		int empHours, empWage;
 
-		int empCheck =(int) Math.floor(Math.random() * 10) % 2;
+		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 		switch (empCheck) {
-			case IS_PRESENT :
-				System.out.println("Employee is Present");
+			case IS_FULL_TIME :
+				System.out.println("Employee is Full Time");
 				empHours = 8;
+				break;
+			case IS_PART_TIME :
+				System.out.println("Employee is Part Time");
+				empHours = 4;
 				break;
 			default :
 				System.out.println("Employee is Absent");
