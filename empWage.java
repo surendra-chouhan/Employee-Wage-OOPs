df74a90f6c367dc5e3dad2881428af9e43c48ed1
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-23 20:22:30 +0530

    Added code to check for attendance

--- empWage.java
+++ empWage.java
@@ -1,5 +1,22 @@
 public class empWage {
+
+	public static final int IS_PRESENT = 1;
+
+	public static void checkAttendance() {
+		int empCheck =(int) Math.floor(Math.random() * 10) % 2;
+		switch (empCheck) {
+			case IS_PRESENT :
+				System.out.println("Employee is Present");
+				break;
+			default :
+				System.out.println("Employee is Absent");
+		}
+
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation problem using OOPs");
+
+		checkAttendance();
 	}
 }
