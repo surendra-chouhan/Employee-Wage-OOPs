3b5c23584f58236f28c956a439747ca57db4154d
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-26 00:29:54 +0530

    Added Code to manage Employee Wage for Multiple Companies

--- empWage.java
+++ empWage.java
@@ -1,76 +1,90 @@
+class CompanyEmpWage {
+	public final String companyName;
+	public final int empWagePerHour;
+	public final int maxWorkingDays;
+	public final int maxWorkingHours;
+	public int totalEmpWage;
+
+	public CompanyEmpWage(String companyName, int empWagePerHour, int maxWorkingDays, int maxWorkingHours) {
+		this.companyName = companyName;
+		this.empWagePerHour = empWagePerHour;
+		this.maxWorkingDays = maxWorkingDays;
+		this.maxWorkingHours = maxWorkingHours;
+	}
+
+	public void setTotalEmpWage(int totalEmpWage){
+		this.totalEmpWage = totalEmpWage;
+	}
+
+	@Override
+	public String toString(){
+		return "Total Wage of : " + companyName + " is : " + totalEmpWage;
+	}
+}
+
 public class empWage {
 
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 
-	private final String companyName;
-	private final int empWagePerHour;
-	private final int maxWorkingDays;
-	private final int maxWorkingHours;
-	private int totalEmpWage;
-	private final int empHoursFullTime;
-	private final int empHoursPartTime;
+	private int numOfCompany = 0;
+	private CompanyEmpWage[] companyEmpWageArray;
 
-	public empWage(String companyName, int empWagePerHour, int maxWorkingDays, int maxWorkingHours, int empHoursFullTime, int empHoursPartTime) {
-		this.companyName = companyName;
-		this.empWagePerHour = empWagePerHour;
-		this.maxWorkingDays = maxWorkingDays;
-		this.maxWorkingHours = maxWorkingHours;
-		this.empHoursFullTime = empHoursFullTime;
-		this.empHoursPartTime = empHoursPartTime;
+	public empWage() {
+		companyEmpWageArray = new CompanyEmpWage[5];
 	}
 
-	public String toString(){
-		return "Total Wage of : " + companyName + " is : " + totalEmpWage;
+	private void addCompanyWage(String companyName, int empWagePerHour, int maxWorkingDays, int maxWorkingHours){
+		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(companyName, empWagePerHour,maxWorkingDays, maxWorkingHours);
+		numOfCompany++;
 	}
 
-	public void calculate_wage() {
+	private void computeWage(){
+		for(int i=0; i<numOfCompany; i++){
+			companyEmpWageArray[i].setTotalEmpWage(this.computeWage(companyEmpWageArray[i]));
+			System.out.println(companyEmpWageArray[i]);
+		}
+	}
+
+
+	public int computeWage(CompanyEmpWage companyEmpWage){
 		int totalWorkingDays = 0;
 		int totalEmpHours = 0;
 		int empHours = 0;
 		int empWage = 0;
 
-		while( totalWorkingDays < maxWorkingDays && totalEmpHours <= maxWorkingHours) {
-		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+		while( totalWorkingDays < companyEmpWage.maxWorkingDays && totalEmpHours <= companyEmpWage.maxWorkingHours) {
+			int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 
-                switch(empCheck) {
-                        case IS_FULL_TIME :
-				empHours = empHoursFullTime;
-                                break;
-                        case IS_PART_TIME :
-                                empHours = empHoursPartTime;
-                                break;
-                        default :
-                                empHours = 0;
-                        }
+			switch(empCheck) {
+				case IS_FULL_TIME :
+					empHours = 8;
+               break;
+            case IS_PART_TIME :
+            	empHours = 4;
+               break;
+            default :
+            	empHours = 0;
+            }
 
 			totalWorkingDays++;
 
-			empWage = empHours * empWagePerHour;
+			empWage = empHours * companyEmpWage.empWagePerHour;
 			System.out.println("Day " + totalWorkingDays + " Wage : " + empWage);
-			totalEmpWage += empWage;
 			totalEmpHours += empHours;
 		}
+		return totalEmpHours * companyEmpWage.empWagePerHour;
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation problem using OOPs");
 
-		empWage Dmart = new empWage("Dmart", 20, 20, 50, 8, 4 );
-		empWage Reliance = new empWage("Reliance Store", 10, 25, 60, 10, 5 );
-		empWage BigBazaar = new empWage("Big Bazaar", 25, 18, 45, 12, 6 );
+		empWage eWage = new empWage();
 
-		System.out.println("Dmart Employee : ");
-		Dmart.calculate_wage();
-		System.out.println(Dmart);
+		eWage.addCompanyWage("Dmart", 20, 20, 50);
+		eWage.addCompanyWage("Reliance Store", 10, 25, 60);
+		eWage.addCompanyWage("Big Bazaar", 25, 18, 45);
 
-		System.out.println("Reliance Employee : ");
-		Reliance.calculate_wage();
-		System.out.println(Reliance);
-
-		System.out.println("Big Bazaar Employee : ");
-		BigBazaar.calculate_wage();
-		System.out.println(BigBazaar);
-
+		eWage.computeWage();
 	}
 }
